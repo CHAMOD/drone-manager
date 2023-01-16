186cd8ac0ff7d13ff5567be690c00791e1d106ef
chamod-pathirana <chamod@productfy.io>
2023-01-16 17:15:21 +0530

    exception handlers

---
+++ src/main/java/com/drone/api/exceptions/DroneNotFoundException.java
@@ -0,0 +1,15 @@
+package com.drone.api.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class DroneNotFoundException extends RuntimeException {
+  public DroneNotFoundException(String message) {
+    super(message);
+  }
+
+  public DroneNotFoundException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
---
+++ src/main/java/com/drone/api/exceptions/InvalidMedicationItemsException.java
@@ -0,0 +1,15 @@
+package com.drone.api.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class InvalidMedicationItemsException extends RuntimeException {
+  public InvalidMedicationItemsException(String message) {
+    super(message);
+  }
+
+  public InvalidMedicationItemsException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
